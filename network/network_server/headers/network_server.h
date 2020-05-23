3f73a2f4a16c318686fa8a20fdc8360a3131c6c4
keithzetterstrom <mariyapmx@mail.ru>
2020-05-23 12:33:49 +0300

    Настроена передача сообщение и сериализация

--- network/network_server/headers/network_server.h
+++ network/network_server/headers/network_server.h
@@ -2,9 +2,13 @@
 #define SCREAMER_NETWORK_SERVER_H
 
 #include <string>
+#include <iostream>
 #include <vector>
 #include "boost/asio.hpp"
-#include "../../tests/headers/test_network_client.h"
+#include <boost/serialization/map.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+//#include "../../tests/headers/test_network_client.h"
 
 enum CmdCodeClient {HELP_CLI,
 		UPLOAD_CLI,
@@ -17,39 +21,41 @@ enum CmdCodeClient {HELP_CLI,
 class ConnectionNetwork
 {
 protected:
-	char * _buf_send;
-	char * _buf_recv;
+	std::string _buf_send;
+	std::string _buf_recv;
 
 private:
-	void Send(int buf_size);
-	void Recv(int buf_size);
-	int Serialize();
-	map<string, string> Deserialize(int buf_size);
+//	void Send(int buf_size);
+//	std::size_t Recv();
+	int Serialize(std::map<std::string, std::string> &server_answer);
+	std::map<std::string, std::string> * Deserialize(int buf_size);
+	//boost::shared_pointer<boost::asio::ip::tcp::socket>
 	boost::asio::ip::tcp::socket * socket;
 
 public:
+	void Send(int buf_size);
+	std::size_t Recv();
 	ConnectionNetwork(boost::asio::ip::tcp::socket *sock);
 	~ConnectionNetwork();
-	void SendMsg(map<string, string> &client_query);
-	map<string, string> RecvMsg();
-	void SendFile(File &file_obj);
-	void RecvFile(File &file_obj_ptr);
+	void SendMsg(std::map<std::string, std::string> &client_query);
+	std::map<std::string, std::string> * RecvMsg();
+	//void SendFile(File &file_obj);
+	//void RecvFile(File &file_obj_ptr);
 };
 
 
 class ServerNetwork
 {
 private:
-	boost::asio::io_context io_context;
-	boost::asio::ip::tcp::acceptor acceptor;
+	boost::asio::io_service _io_service;
+	boost::asio::ip::tcp::acceptor * _acceptor;
 	int _port;
-	string _IP;
+	std::string _IP;
 
 public:
-	ServerNetwork(boost::asio::io_context io_context);
+	ServerNetwork(const std::string& IP, int port);
 	~ServerNetwork();
 	ConnectionNetwork StandConnection();
 };
-
 
 #endif //SCREAMER_NETWORK_SERVER_H
